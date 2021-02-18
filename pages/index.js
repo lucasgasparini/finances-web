409190659c447862bcbaac2584d120cd02077805
Lucas <lucasgasparinibr@gmail.com>
2021-02-18 14:58:00 -0300

    :lipstick: Implement test home page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,50 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [expenseValue, setExpenseValue] = useState('');
+  const [expensesList, setExpensesList] = useState([]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addValueToExpensesList(expenseValue);
+    }
+  };
+
+  const addValueToExpensesList = () => {
+    const newList = [...expensesList];
+    newList.push(expenseValue);
+    setExpensesList(newList);
+    setExpenseValue("");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Finances</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>How much have you spent?</h1>
+        <input
+          name="expenseValue"
+          className={styles.input}
+          placeholder="$$$$"
+          value={expenseValue}
+          onChange={(e) => setExpenseValue(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e)}
+        />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="styles.expensesList">
+          {expensesList.map((expense, index) => (
+            <p key={index}>{expense}</p>
+          ))}
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
