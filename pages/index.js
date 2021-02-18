69e80fa5071a2ee040d1e4e2f1a3e7f55f9a4319
Lucas <lucasgasparinibr@gmail.com>
2021-02-18 15:33:54 -0300

    :art: Add prettier file and indent code

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,51 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [expenseValue, setExpenseValue] = useState('');
-  const [expensesList, setExpensesList] = useState([]);
+    const [expenseValue, setExpenseValue] = useState('')
+    const [expensesList, setExpensesList] = useState([])
 
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      addValueToExpensesList(expenseValue);
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            addValueToExpensesList(expenseValue)
+        }
     }
-  };
 
-  const addValueToExpensesList = () => {
-    const newList = [...expensesList];
-    newList.push(expenseValue);
-    setExpensesList(newList);
-    setExpenseValue("");
-  };
+    const addValueToExpensesList = () => {
+        const newList = [...expensesList]
+        newList.push(expenseValue)
+        setExpensesList(newList)
+        setExpenseValue('')
+    }
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Finances</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Finances</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>How much have you spent?</h1>
-        <input
-          name="expenseValue"
-          className={styles.input}
-          placeholder="$$$$"
-          value={expenseValue}
-          onChange={(e) => setExpenseValue(e.target.value)}
-          onKeyDown={(e) => handleKeyDown(e)}
-        />
+            <main className={styles.main}>
+                <h1 className={styles.title}>How much have you spent?</h1>
+                <input
+                    name="expenseValue"
+                    className={styles.input}
+                    placeholder="$$$$"
+                    value={expenseValue}
+					type="number"
+                    onChange={(e) => setExpenseValue(e.target.value)}
+                    onKeyDown={(e) => handleKeyDown(e)}
+                />
 
-        <div className="styles.expensesList">
-          {expensesList.map((expense, index) => (
-            <p key={index}>{expense}</p>
-          ))}
+                <div className="styles.expensesList">
+                    {expensesList.map((expense, index) => (
+                        <p key={index}>{expense}</p>
+                    ))}
+                </div>
+            </main>
+
+            <footer className={styles.footer}></footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}></footer>
-    </div>
-  );
+    )
 }
